8841804aed26e0ddf5c85603617754aab686fc1c
michal.lester87@gmail.com <michal.lester87@gmail.com>
2018-12-20 00:04:14 +0100

    5 finished

--- src/index.js
+++ src/index.js
@@ -2,40 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from './LoginPage';
 import MainPage from './MainPage';
-import UserContext from './UserContext';
-import { FAKE_USER } from './api';
+import { UserProvider, UserConsumer } from './UserContext';
+import { EmailProvider, EmailConsumer } from './EmailContext';
 import './index.css';
 
-class Root extends React.Component {
-	state = {
-		currentUser: FAKE_USER
-	};
-
-	handleLogin = (user) => {
-		this.setState({ currentUser: user });
-	};
-
-	handleLogout = () => {
-		this.setState({ currentUser: null });
-	};
-
-	render() {
-		return (
-			<UserContext.Provider
-				value={{
-					user: this.state.currentUser,
-					onLogin: this.handleLogin,
-					onLogout: this.handleLogout
-				}}
-			>
-				{this.state.currentUser ? (
-					<MainPage />
-				) : (
-					<LoginPage onLogin={this.handleLogin} />
-				)}
-			</UserContext.Provider>
-		);
-	}
+function Root() {
+	return (
+		<UserConsumer>
+			{({ user }) =>
+				user ? <MainPage /> : <LoginPage onLogin={this.handleLogin} />}
+		</UserConsumer>
+	);
 }
 
-ReactDOM.render(<Root />, document.querySelector('#root'));
+ReactDOM.render(
+	<UserProvider>
+		<EmailProvider>
+			<Root />
+		</EmailProvider>
+	</UserProvider>,
+	document.querySelector('#root')
+);
